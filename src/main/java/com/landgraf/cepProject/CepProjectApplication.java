6d2ec5a48dc79b5be862468fac0dad2187cd142e
Thomas Landgraf <thomas.ml@hotmail.com>
2026-04-25 16:32:32 -0700

    First commit

---
+++ src/main/java/com/landgraf/cepProject/CepProjectApplication.java
@@ -0,0 +1,13 @@
+package com.landgraf.cepProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/landgraf/cepProject/CepProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landgraf.cepProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
